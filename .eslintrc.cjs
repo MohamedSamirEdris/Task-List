650787e101120ff2843d876a2b1ad4044d72a3ed
mohamedsamir <mohamedsamiredries@outlook.com>
2024-04-12 13:03:41 +0200

    Feat: Add eslint for unused packages

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,8 +8,9 @@ module.exports = {
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-  plugins: ["react-refresh"],
+  plugins: ["react-refresh", "unused-imports"],
   rules: {
     "react-refresh/only-export-components": "warn",
+    "unused-imports/no-unused-imports": "error",
   },
 };
